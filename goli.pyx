4a60d0a43ab801ade8c60bbaeaff68041c8ad412
ogshivam <123156173+ogshivam@users.noreply.github.com>
2024-05-10 09:48:27 +0530

    Update goli.pyx

--- goli.pyx
+++ goli.pyx
@@ -1,12 +1,3 @@
-def factorial(n: int) -> int:
-    """
-    Vanilla Python function to calculate the factorial of a given number.
-    """
-    result: int = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
 cpdef int factorial_cython(int n):
     """
     Cythonized version of the factorial function with types added.
