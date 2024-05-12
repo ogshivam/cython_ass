6ba7c262f37da70a905da97ed81d5cb40d827a27
ogshivam <123156173+ogshivam@users.noreply.github.com>
2024-05-12 22:53:23 +0530

    Delete shivam.py

--- shivam.py
+++
@@ -1,8 +0,0 @@
-def factorial(n: int) -> int:
-    """
-    Vanilla Python function to calculate the factorial of a given number.
-    """
-    result: int = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
