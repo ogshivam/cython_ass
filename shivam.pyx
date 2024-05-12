a6507124eddc2afa09373e5ba2cc9d186fa6660d
ogshivam <123156173+ogshivam@users.noreply.github.com>
2024-05-12 22:53:53 +0530

    Delete shivam.pyx

--- shivam.pyx
+++
@@ -1,9 +0,0 @@
-cpdef int factorial_cython(int n):
-    """
-    Cythonized version of the factorial function with types added.
-    """
-    cdef int result = 1
-    cdef int i
-    for i in range(1, n + 1):
-        result *= i
-    return result
